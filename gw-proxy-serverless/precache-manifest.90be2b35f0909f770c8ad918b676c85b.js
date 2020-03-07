a417ae68e552270d5d05c6db39640e9348c6de82
serhii226 <myzsoftdev1@gmail.com>
2020-03-07 14:34:38 -0500

    Initial commit

---
+++ gw-proxy-serverless/precache-manifest.90be2b35f0909f770c8ad918b676c85b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c1f5239303829b0662811c65a03731",
+    "url": "/gw-proxy-serverless/index.html"
+  },
+  {
+    "revision": "7c321395ef30f830e2ed",
+    "url": "/gw-proxy-serverless/static/css/2.563af9af.chunk.css"
+  },
+  {
+    "revision": "95c6df84649015bfcaf7",
+    "url": "/gw-proxy-serverless/static/css/main.e0bbd16a.chunk.css"
+  },
+  {
+    "revision": "7c321395ef30f830e2ed",
+    "url": "/gw-proxy-serverless/static/js/2.af3a46c7.chunk.js"
+  },
+  {
+    "revision": "e6e659ede2983e3ba57911926b8d9c10",
+    "url": "/gw-proxy-serverless/static/js/2.af3a46c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c6df84649015bfcaf7",
+    "url": "/gw-proxy-serverless/static/js/main.0898cd90.chunk.js"
+  },
+  {
+    "revision": "1a7b80f30a58d7879d4f",
+    "url": "/gw-proxy-serverless/static/js/runtime-main.13d7f748.js"
+  }
+]);
